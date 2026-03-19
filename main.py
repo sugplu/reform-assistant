edcb13a642f418df11aa909c956d3b83b4a86639
sugplu <josephlinares02@gmail.com>
2026-03-18 17:00:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -490,3 +490,6 @@ if __name__ == "__main__":
 
     port = int(os.getenv("PORT", "8000"))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
+"- Always write full absolute links starting with https:// .\n"
+
